f76b4ab0e187b289916ee406f20d4faac5c025a7
b2230765003 <b2230765003@cs.hacettepe.edu.tr>
2025-07-31 04:32:52 +0300

    chore: switch DATABASES to dj_database_url

--- dentalsozluk/settings.py
+++ dentalsozluk/settings.py
@@ -122,13 +122,16 @@ WSGI_APPLICATION = 'dentalsozluk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+# Önceki DATABASES ayarını tamamen kaldırın ve yerine:
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
